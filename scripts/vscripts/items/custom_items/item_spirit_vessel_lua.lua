ec657437845b3993d72b84965674b76eecdc66f9
iwasinminedream <iwasinminedream@gmail.com>
2023-11-05 19:31:08 +0500

    vesel

--- scripts/vscripts/items/custom_items/item_spirit_vessel_lua.lua
+++ scripts/vscripts/items/custom_items/item_spirit_vessel_lua.lua
@@ -10,6 +10,15 @@ function item_spirit_vessel_lua:OnSpellStart()
     EmitSoundOn("DOTA_Item.SpiritVessel.Target.Enemy", self:GetCursorTarget())
 end
 
+function item_spirit_vessel_lua:GetAbilityTextureName()
+	local level = self:GetLevel()
+	if self:GetSecondaryCharges() == 0 then
+		return "all/veil_" .. level
+	else
+		return "gem" .. self:GetSecondaryCharges() .. "/item_veil_of_discord_lua" .. level
+	end
+end
+
 function item_spirit_vessel_lua:GetIntrinsicModifierName()
     return "modifier_item_spirit_vessel_lua"
 end
